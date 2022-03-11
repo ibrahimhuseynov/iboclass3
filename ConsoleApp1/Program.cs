da337986b977b741bd518b9b6331c222dccbd1e6
ibrahimhuseynov <tu7pdt8l8@code.edu.az>
2022-03-11 07:40:55 +0400

    creat class one

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            car cr=new car();
+            cr.name = "bmw";
+            cr.color = "black";
+            cr.model = "f10";
+            cr.brand = "Germany";
+            Console.WriteLine("name :"+cr.name);
+            Console.WriteLine("color :" + cr.color);
+            Console.WriteLine("model :"+cr.model);
+            Console.WriteLine("brand :"+cr.brand);
+            Console.Read();
+        }
+    }
+}
